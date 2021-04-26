8e354223360d0b0063f50cf0982a39946e0f2599
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-04-26 04:29:16 -0700

    luis

--- Project/Services/CRUDClientes.cs
+++ Project/Services/CRUDClientes.cs
@@ -5,11 +5,13 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using SuperAlquiler.Entities;
+using System.IO;
 
 namespace SuperAlquiler.Services
 {
     public class CRUDClientes
     {
+        string ruta;
         Conexion conexion = null;
 
         private void InstanciarConexion()
@@ -31,12 +33,22 @@ namespace SuperAlquiler.Services
         public void Create (Cliente cliente)
         {
             InstanciarConexion();
+            
+            //FileStream stream = new FileStream(ruta, FileMode.Open, FileAccess.Read);
+            //BinaryReader br = new BinaryReader(stream);
+            //FileInfo fi = new FileInfo(ruta);
+
+
+            //byte[] binData = new byte[stream.Length];
+
+            //stream.Read(binData, 0, Convert.ToInt32(stream.Length));
+
+            //cliente.Foto = Image.FromStream(stream);
 
             SqlConnection con = conexion.Conectar();
 
             SqlCommand cmd = new SqlCommand("SP_INSERTAR_CLIENTES", con);
             cmd.CommandType = CommandType.StoredProcedure;
-
             cmd.Parameters.AddWithValue("@CEDULA", cliente.Cedula);
             cmd.Parameters.AddWithValue("@NOMBRES", cliente.Nombre);
             cmd.Parameters.AddWithValue("@APELLIDOS", cliente.Apellido);
@@ -44,9 +56,8 @@ namespace SuperAlquiler.Services
             cmd.Parameters.AddWithValue("@LICENCIA", cliente.Licencia);
             cmd.Parameters.AddWithValue("@NACIONALIDAD", cliente.Nacionalidad);
             cmd.Parameters.AddWithValue("@TIPO_DE_SANGRE", cliente.TipoDeSangre);
-            cmd.Parameters.AddWithValue("@FOTO_CLIENTE", cliente.FotoLicencia);
-            cmd.Parameters.AddWithValue("@FOTO_CEDULA", cliente.Foto);
-
+            //cmd.Parameters.AddWithValue("@FOTO_CLIENTE", cliente.Foto);
+            //cmd.Parameters.AddWithValue("@FOTO_CEDULA", cliente.FotoLicencia);
             cmd.ExecuteNonQuery();
 
             conexion.Desconectar();
@@ -65,17 +76,18 @@ namespace SuperAlquiler.Services
 
             while (reader.Read())
             {
+                
                 Cliente cliente = new Cliente();
-
-                cmd.Parameters.AddWithValue("@CEDULA", cliente.Cedula);
-                cmd.Parameters.AddWithValue("@NOMBRES", cliente.Nombre);
-                cmd.Parameters.AddWithValue("@APELLIDOS", cliente.Apellido);
-                cmd.Parameters.AddWithValue("@CORREO", cliente.Correo);
-                cmd.Parameters.AddWithValue("@LICENCIA", cliente.Licencia);
-                cmd.Parameters.AddWithValue("@NACIONALIDAD", cliente.Nacionalidad);
-                cmd.Parameters.AddWithValue("@TIPO_DE_SANGRE", cliente.TipoDeSangre);
-                cmd.Parameters.AddWithValue("@FOTO_CLIENTE", cliente.FotoLicencia);
-                cmd.Parameters.AddWithValue("@FOTO_CEDULA", cliente.Foto);
+                cliente.Id = reader.GetInt32("ID_CLIENTE");
+                cliente.Cedula = reader.GetString("CEDULA");
+                cliente.Nombre = reader.GetString("NOMBRES");
+                cliente.Apellido = reader.GetString("APELLIDOS");
+                cliente.Correo = reader.GetString("CORREO");
+                cliente.Licencia = reader.GetString("LICENCIA");
+                cliente.Nacionalidad = reader.GetString("NACIONALIDAD");
+                cliente.TipoDeSangre = reader.GetString("TIPO_DE_SANGRE");
+                //cliente.FotoLicencia = (Byte[])reader["FOTO_CEDULA"];
+                //cliente.Foto = (Byte[])reader["FOTO_CLIENTE"];
 
                 lista.Add(cliente);
             }
@@ -100,8 +112,8 @@ namespace SuperAlquiler.Services
             cmd.Parameters.AddWithValue("@LICENCIA", cliente.Licencia);
             cmd.Parameters.AddWithValue("@NACIONALIDAD", cliente.Nacionalidad);
             cmd.Parameters.AddWithValue("@TIPO_DE_SANGRE", cliente.TipoDeSangre);
-            cmd.Parameters.AddWithValue("@FOTO_CLIENTE", cliente.FotoLicencia);
-            cmd.Parameters.AddWithValue("@FOTO_CEDULA", cliente.Foto);
+            //cmd.Parameters.AddWithValue("@FOTO_CLIENTE", cliente.Foto);
+            //cmd.Parameters.AddWithValue("@FOTO_CEDULA", cliente.FotoLicencia);
 
             cmd.ExecuteNonQuery();
 
