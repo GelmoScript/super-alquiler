054f3822d74d9db8d6e6d3c6f03febe41fc18ef8
Alejandro Germosen <gelmoscript@gmail.com>
2021-04-25 20:52:24 -0400

    Todos los reportes agregados

---
+++ Project/Entities/ClienteQueDebe.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SuperAlquiler.Entities
+{
+    public class ClienteQueDebe
+    {
+        public Cliente Cliente { get; set; }
+        public double Monto { get; set; }
+    }
+}
